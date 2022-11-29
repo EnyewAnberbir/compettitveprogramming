6430ac94fe76b3ecc38175bb547ec65aadd630de
EnyewAnberbir <119450772+EnyewAnberbir@users.noreply.github.com>
2022-11-30 00:24:24 +0300

    Create Task Scheduler.py

---
+++ Task Scheduler/Task Scheduler.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def leastInterval(self, tasks, n):
+        task_counts = collections.Counter(tasks).values()
+        M = max(task_counts)
+        Mct = task_counts.count(M)
+        return max(len(tasks), (M - 1) * (n + 1) + Mct)
+        
