a4a67504ffdf07f84bb3c62d75f2a58d432a00ad
EnyewAnberbir <119450772+EnyewAnberbir@users.noreply.github.com>
2022-12-02 17:09:35 +0300

    Create Find Pivot Index.py

---
+++ Find Pivot Index/Find Pivot Index.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        left, right = 0, sum(nums)
+        for index, num in enumerate(nums):
+            right -= num
+            if left == right:
+                return index
+            left += num
+        return -1
